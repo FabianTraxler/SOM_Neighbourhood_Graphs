91274b5eaa710764ee435a72589bb43300d8670f
Fabsl22 <fabi.traxler@hotmail.com>
2021-01-22 19:03:48 +0100

    Initial Commit

---
+++ NeighbourhoodGraph.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class DistanceMatrix:
+
+    def __init__(self, input_data=None):
+        self.distances = None
+        
+
+    def calculate_distances(self, input_data):
+
+    
